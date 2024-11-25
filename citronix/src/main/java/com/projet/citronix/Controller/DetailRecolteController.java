e80e4df76ae62de871cf38760b317d083bd5d91b
Walid Saifi <saifiwalid21@gmail.com>
2024-11-25 11:23:03 +0100

    ajouter Gestion DetailRecolte

--- citronix/src/main/java/com/projet/citronix/Controller/DetailRecolteController.java
+++ citronix/src/main/java/com/projet/citronix/Controller/DetailRecolteController.java
@@ -3,15 +3,13 @@ package com.projet.citronix.Controller;
 
 import com.projet.citronix.Dto.Request.DetailRecolteRequestDTO;
 import com.projet.citronix.Dto.Response.DetailRecolteReponseDTO;
+import com.projet.citronix.Exception.DetailRecolteNonTrouverException;
 import com.projet.citronix.Exception.RecolteNonTrouveeException;
 import com.projet.citronix.Service.DetailRecolteService;
 import com.projet.citronix.entity.DetailRecolte;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -36,7 +34,28 @@ public class DetailRecolteController {
     @PostMapping
     public ResponseEntity<HashMap<String, Object>> ajouterDetailRecolte(@RequestBody DetailRecolteRequestDTO detailRecolteRequestDTO) throws RecolteNonTrouveeException {
             DetailRecolteReponseDTO detailRecolteResponseDTO = detailRecolteService.Create_DetailRecolte(detailRecolteRequestDTO);
-            return ResponseEntity.status(201).body(createMessage("Détail de récolte ajouté avec succès", detailRecolteResponseDTO, 201));
+            return ResponseEntity.status(201).body(createMessage("Détail de récolte ajouté avec succès.", detailRecolteResponseDTO, 201));
     }
 
+    @PutMapping("/modifier/{id}")
+    public ResponseEntity<HashMap<String,Object>> Modifier_DetailRecole(@PathVariable Long id ,@RequestBody DetailRecolteRequestDTO detailRecolteRequestDTO) {
+        try {
+            DetailRecolteReponseDTO  detailRecolteReponseDTO = detailRecolteService.Modification_DetailRecolte(id,detailRecolteRequestDTO);
+            return ResponseEntity.status(201).body(createMessage("Détail de récolte Modifier avec succès.", detailRecolteReponseDTO, 201));
+        } catch (RecolteNonTrouveeException ex) {
+            return ResponseEntity.status(404).body(createMessage(ex.getMessage(), null, 404));
+        }
+    }
+    @
+    DeleteMapping("/{id}")
+    public ResponseEntity<HashMap<String,Object>> Supprimer_DetailRecolte(@PathVariable Long id) {
+        try {
+            detailRecolteService.supprimer_DetailRecolte(id);
+             return ResponseEntity.ok(createMessage("Detail Recole supprimée avec succès", null, 200));
+        } catch (DetailRecolteNonTrouverException e) {
+            return ResponseEntity.status(404).body(createMessage("Detail Recole non trouvée pour suppression", null, 404));
+        }
+
+
+    }
 }
